6d439b98b9bb96ca199e9d1aa2103461d3a9d525
Ken Wang <kenwang0615@gmail.com>
2013-05-20 22:04:52 +0800

    aaaa

--- AndroidTesttee/src/com/sks/androidtesttee/SecondActivity.java
+++ AndroidTesttee/src/com/sks/androidtesttee/SecondActivity.java
@@ -1,7 +1,14 @@
 package com.sks.androidtesttee;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class SecondActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
 
 }
